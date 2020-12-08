00e88653b663208574d7dac5dc9140015aea5788
Aleksander Dzierżanowski <a.dzierzanowski1@gmail.com>
2020-12-08 17:03:22 +0100

    day8

---
+++ README.md
@@ -0,0 +1,2 @@
+I didn't post day7 solution because I'm too embarassed for what I did there.
+I'll maybe try later and clean it up.
---
+++ day8.py
@@ -0,0 +1,39 @@
+from common.cpu import CPU, EndOfCodeError, RepeatedInstructionError
+from helpers import readlines, rpath, tpath
+
+
+def part1(data):
+  cpu = CPU(debug_mode=False)
+  cpu.load_code(data)
+
+  try:
+    cpu.run()
+  except RepeatedInstructionError:
+    pass
+
+  return cpu.acc
+
+def part2(data):
+  for i, line in enumerate(data):
+    newcode = data[:]
+
+    if 'jmp' in line:
+      newcode[i] = newcode[i].replace('jmp', 'nop')
+
+    elif 'nop' in line:
+      newcode[i] = newcode[i].replace('nop', 'jmp')
+
+    cpu = CPU(debug_mode=False)
+    cpu.load_code(newcode)
+
+    try:
+      cpu.run()
+    except RepeatedInstructionError:
+      pass
+    except EndOfCodeError:
+      return cpu.acc
+
+if __name__ == '__main__':
+  data = readlines(rpath('day8.txt'))
+  print(part1(data))
+  print(part2(data))
