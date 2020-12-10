f8e356409cf28c5eb4e071aa478ab560d3e76063
Aleksander Dzierżanowski <a.dzierzanowski1@gmail.com>
2020-12-10 08:34:10 +0100

    formatting

--- day10.py
+++ day10.py
@@ -13,13 +13,12 @@ def part1(data):
   return diffs.count(1) * diffs.count(3)
 
 def part2(data):
-  diffs = get_diffs(data)
-  diffs = ''.join([str(x) for x in diffs])
-  ones = [d for d in diffs.split('3') if d != '' and d != '1']
+  diffs = ''.join([str(x) for x in get_diffs(data)])
+  ones = [d for d in diffs.split('3') if d not in ('', '1')]
 
   # Ways we can substitute differences of 1 with differences of 2 or 3
   # (plus 1 for no change at all)
-  # 11    -> 2  (11, 2)
+  # 11    -> 2 (11, 2)
   # 111   -> 4 (111, 21, 12, 3)
   # 1111  -> 7 (1111, 211, 112, 22, 31, 13)
 
