418effbcd18d33f9799cff32ce1634cde2bfd9ab
Aleksander Dzierżanowski <a.dzierzanowski1@gmail.com>
2020-12-16 22:32:02 +0100

    day16

--- day16.py
+++ day16.py
@@ -1,46 +1,61 @@
+from functools import reduce
 from itertools import chain
+from operator import mul
 
 from helpers import readlines, rpath, tpath
 
 
-def parse_range(s):
-  s = s.split(' or ')
-  s1, s2 = s[0].split('-'), s[1].split('-')
-  r1 = range(int(s1[0]), int(s1[1])+1)
-  r2 = range(int(s2[0]), int(s2[1])+1)
-  return r1, r2
+def parse_range(rngstr):
+  r1str, r2str = rngstr.split(' or ')
+  r1a, r1b = ([int(x) for x in r1str.split('-')])
+  r2a, r2b = ([int(x) for x in r2str.split('-')])
+
+  return tuple(chain(range(r1a, r1b+1), range(r2a, r2b+1)))
 
 def parse(data):
-  ranges = [s.split(': ') for s in data[0].split('\n')]
-  ranges = {s[0]: parse_range(s[1]) for s in ranges}
-  my_ticket = [int(x) for x in data[1].split('\n')[1].split(',')]
-  tickets = [[int(x) for x in line.split(',')] for line in data[2].split('\n')[1:]]
-  return ranges, my_ticket, tickets
+  ranges = {k: parse_range(v) for k, v in [l.split(': ') for l in data[0]] }
+  mytick = [int(x) for x in data[1][1].split(',')]
+  tickets = [[int(x) for x in t.split(',')] for t in data[2][1:]]
+  allrng = tuple(chain(*ranges.values()))
+  return ranges, mytick, tickets, allrng
 
-def part1(ranges, tickets):
-  rng = list(chain(*[chain(r1, r2) for r1, r2 in ranges.values()]))
-  all_tickets = sum(tickets, [])
-  return sum((x for x in all_tickets if x not in rng))
+def part1(ranges, tickets, allrng):
+  s = 0
+  for t in tickets:
+    for v in t:
+      if v not in allrng:
+        s += v
+  return s
 
-def part2(ranges, tickets):
-  rng = list(chain(*[chain(r1, r2) for r1, r2 in ranges.values()]))
+def part2(ranges, tickets, allrng, mytick):
+  field_cnt = len(tickets[0])
 
-  ts = [t for t in tickets if all([x in rng for x in t])]
-  ts_fields = [[t[i] for t in ts] for i in range(len(ts[0]))]
+  # Filter valid tickets, assing a name of range to a string representing
+  # if given field is valid across all tickets
+  validts = [t for t in tickets if all((v in allrng for v in t))]
+  valid_cols = [[v[i] for v in validts] for i in range(field_cnt)]
+  rng_cols = {rname: ''.join([str(int(all((v in rng for v in col))))
+                             for i, col in enumerate(valid_cols)])
+              for rname, rng in ranges.items()}
 
-  fields = {k: None for k in ranges}
+  # Now if a given field is valid in only one column across all tickets,
+  # then that must be it, we save that information and remove the column
+  # marked as valid from remaining fields
+  field_cols = {}
+  while len(field_cols) != field_cnt:
+    field, val = [(f, v) for f, v in rng_cols.items() if v.count('1') == 1][0]
+    field_cols[field] = val.index('1')
+    rng_cols = {rname: f'{int(c, 2) & ~int(val, 2):020b}'
+                for rname, c in rng_cols.items()}
 
-  for rname, r in ranges.items():
-    rng = list(chain(*r))
-    for i, fld in enumerate(ts_fields):
-      if all([x in rng for x in fld]):
-        fields[rname] = i
-        break
-  print(fields)
+  my_fields = {k: v for k, v in field_cols.items() if k.startswith('departure')}
+  res = [mytick[i] for i in my_fields.values()]
+  return reduce(mul, res)
+
 
 if __name__ == "__main__":
-  data = readlines(rpath('day16.txt'), sep='\n\n')
-  ranges, my_ticket, tickets = parse(data)
+  data = [d.split('\n') for d in readlines(rpath('day16.txt'), sep='\n\n')]
+  ranges, mytick, tickets, allrng = parse(data)
 
-  print(part1(ranges, tickets))
-  print(part2(ranges, tickets))
+  print(part1(ranges, tickets, allrng))
+  print(part2(ranges, tickets, allrng, mytick))
