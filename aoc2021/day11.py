20de83bdb1f6de3c266a781baf490f76944c1de8
Aleksander Dzierżanowski <a.dzierzanowski1@gmail.com>
2021-12-11 07:22:44 +0100

    day11 cleanup

--- aoc2021/day11.py
+++ aoc2021/day11.py
@@ -10,47 +10,32 @@ def adjacent(points, data):
           adj.append((x+dx, y+dy))
   return adj
 
-def draw(data):
-  for y in range(10):
-    for x in range(10):
-      if data[x, y] == 0:
-        print(f'\x1b[38;5;2m{data[x,y]}\x1b[0m', end='')
-      else:
-        print(data[x,y], end='')
-    print()
-  print()
-
 def step(data):
   whoflashed = []
   for pos, energy in data.items():
     data[pos] += 1
     if energy == 9:
       whoflashed.append(pos)
-      data[pos] = 0
-  #draw(data)
 
   newflashes = whoflashed
   while adj := adjacent(newflashes, data):
-    #print(adj)
     newflashes = []
     for pos in adj:
       data[pos] += 1
       if data[pos] == 10:
         whoflashed.append(pos)
         newflashes.append(pos)
-        data[pos] = 0
-    #draw(data)
 
   for pos in whoflashed:
     data[pos] = 0
-  #draw(data)
+
   return len(whoflashed)
 
 
 def part1(data):
   d = {**data}
   flashcnt = 0
-  for _ in range(195):
+  for _ in range(100):
     flashcnt += step(d)
   return flashcnt
 
